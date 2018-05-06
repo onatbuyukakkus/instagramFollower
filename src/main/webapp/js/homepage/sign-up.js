11232dd20ae65631d89219ede3c5cce709a4c196
onatbuyukakkus <onatbuyukakkus@gmail.com>
2018-05-06 05:54:02 +0200

    small issue resolved

--- src/main/webapp/js/homepage/sign-up.js
+++ src/main/webapp/js/homepage/sign-up.js
@@ -7,8 +7,6 @@ $(document).ready(function(){
             type: "POST",
             url: "http://139.59.135.39:9090/instagram-follower-1.0-SNAPSHOT/rest/user/add?username=" + username + "&password=" + password,
             success: function(response){
-                console.log(response);
-                console.log(response.text]);
                 $("#response_message").html("</br><b>" + response.text + "</b>");
             },
             error: function(xhr) {
